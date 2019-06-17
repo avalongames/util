0807a2a42eeff877324f8e52874cc9aacccaf67b
xumuxi <xumuxi@avalongames.cn>
2019-06-17 14:55:03 +0800

    添加web端接口标准输出类

---
+++ src/StandardResponse.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xmxtc
+ * Date: 2019/6/4
+ * Time: 3:52 PM
+ */
+
+namespace AvalonGames\Util;
+
+
+class StandardResponse
+{
+    private $status = 0;
+
+    private $data;
+
+    private $links;
+
+    public function response($result=null)
+    {
+        $this -> setData($result);
+
+        return response() -> json($this -> getResponseData());
+    }
+
+    protected function getResponseData()
+    {
+        return [
+            'status'    => $this -> status,
+            'data'      => $this -> data,
+            '_links'    => $this -> getLinks()
+        ];
+    }
+
+    public function setStatus(int $status)
+    {
+        $this -> status = $status;
+    }
+
+    public function getLinks()
+    {
+        if(!$this -> links) {
+            $this -> links = [
+                'self'  => [
+                    'href'  => url() -> current()
+                ]
+            ];
+        }
+
+        return $this -> links;
+    }
+
+    public function setLinks($links)
+    {
+        $this -> links = $links;
+    }
+
+    public function setData($data)
+    {
+        $this -> data = $data;
+    }
+}
